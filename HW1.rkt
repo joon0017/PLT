ced2eea910f65be1f07a7506f2d014a2e247b286
Sniree <97450065+joon0017@users.noreply.github.com>
2023-09-05 16:51:09 +0900

    prob8

--- HW1.rkt
+++ HW1.rkt
@@ -16,7 +16,7 @@
 ;Problem 2
 ;Solved by myself.
 ;time taken around 5 mins
-;[contract] max-of-three-integers, 3 numbers -> number
+;[contract] max-of-three-integers, number number number -> number
 ;[purpose] find the largest number out of 3 integer numbers
 ;[tests] (test (max-of-three-integers 1 2 3) 3)
 ;(test (max-of-three-integers 50 20 10) 50)
@@ -38,7 +38,7 @@
 ;Problem 3
 ;Solved by myself
 ;time taken around 3 mins
-;[contract] volume-cuboid, 3 numbers -> number
+;[contract] volume-cuboid, number number number -> number
 ;[purpose] find the volume of a cuboid using 3 given numbers
 ;[tests] (test (volume-cuboid 1 5 2) 10)
 ;        (test (volume-cuboid 5 10 20) 1000)
@@ -54,7 +54,7 @@
 ;Problem 4
 ;Solved with the help of this website: https://dyclassroom.com/recursion-algorithm/greatest-common-divisor-gcd
 ;time taken around 3 mins
-;[contract] myGcd, 2 numbers -> 1 number
+;[contract] myGcd, number number -> number
 ;[purpose] find the gcd of 2 integer values
 ;[tests] (test (myGcd 15 25) 5)
 ;        (test (myGcd 10 8) 2)
@@ -73,7 +73,7 @@
 ;Problem 5
 ;Solved by myself
 ;time taken around 7 mins
-;[contract] combination, 2 numbers -> number
+;[contract] combination, number number -> number
 ;[purpose] To find the number of combinations with the 2 numbers given
 ;[tests] (test (combination 10 2)45)
 ;        (test (combination 5 3)10)
@@ -141,19 +141,19 @@ myAirplane
 
 ;c)
 (define (is-vehicle-safe v)
-  (type-case Vehicle v
-    [Bicycle (wheel)
+  (type-case Vehicle v ;check each type separately
+    [Bicycle (wheel) ;bike
      (cond
        [(< wheel 4) "safe"]
        [else "unsafe"])]
-    [Car (wheel window)
+    [Car (wheel window) ;car
       (cond
         [(> wheel 3) (cond
                        [(> window 2) "safe"]
                        [else "unsafe"])
                      ]
         [else "unsafe"])]
-    [Airplane (wheel window engine)
+    [Airplane (wheel window engine) ;airplane
       (cond
         [(> wheel 2) (cond
                        [(> window 10) (cond
@@ -185,10 +185,10 @@ myAirplane
   (cond [(empty? lst) empty]
         
         ; only check for a, c, j, k
-        [(equal? (first lst) 'a) (append '(alice) (name-alphabet(rest lst)))]
-        [(equal? (first lst) 'c) (append '(cherry) (name-alphabet(rest lst)))]
-        [(equal? (first lst) 'j) (append '(jc) (name-alphabet(rest lst)))]
-        [(equal? (first lst) 'k) (append '(kate) (name-alphabet(rest lst)))]
+        [(equal? (first lst) 'a) (cons 'alice (name-alphabet(rest lst)))]
+        [(equal? (first lst) 'c) (cons 'cherry (name-alphabet(rest lst)))]
+        [(equal? (first lst) 'j) (cons 'jc (name-alphabet(rest lst)))]
+        [(equal? (first lst) 'k) (cons 'kate (name-alphabet(rest lst)))]
         
         [else (append '(unnamed) (name-alphabet(rest lst)))]
    ))
@@ -196,11 +196,23 @@ myAirplane
 (test (name-alphabet '(a b c i j k))'(alice unnamed cherry unnamed jc kate))
 (test (name-alphabet '(k j i c b a x))'(kate jc unnamed cherry unnamed alice unnamed))
 
-;Problem 
-;Solved by 
-;time taken around 
-;[contract] 
-;[purpose] 
-;[tests] (test 
-;        (test 
-;        (test 
+;Problem 8
+;Solved by myself
+;time taken around 5~10 mins
+;[contract] update-name, symbol symbol list -> list
+;[purpose] Return a list that has all old symbols replaced with new symbols
+;[tests] (test (update-name 'cherry 'claire (cons 'jc (cons 'cherry (cons 'kate empty)))) '(jc claire kate))
+;        (test (update-name 'alice 'aaron '(sj alice cherry kate)) '(sj aaron cherry kate))
+
+
+(define (update-name old new lst) ;recursively check whole list, one by one
+  (cond [(empty? lst) empty]
+
+        ;check for replacements
+        [(equal? (first lst) old) (cons new (update-name old new (rest lst)))]
+
+        ;else return original symbol
+        [else (cons (first lst) (update-name old new (rest lst)))]))
+
+(test (update-name 'cherry 'claire (cons 'jc (cons 'cherry (cons 'kate empty)))) '(jc claire kate))
+(test (update-name 'alice 'aaron '(sj alice cherry kate)) '(sj aaron cherry kate))
